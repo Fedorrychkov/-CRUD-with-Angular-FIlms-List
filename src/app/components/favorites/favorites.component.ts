243433a083ce677c6bc09f47cd44804bc174160f
Fedorrychkov <fedorrychkov@yandex.ru>
2018-03-24 22:11:09 +0300

    Add favorites component

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IFilmShort } from '../../interfaces/IFilmShort';
+import { FavoriteService } from '../../services/favorite.service';
 
 @Component({
   selector: 'app-favorites',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.component.scss']
 })
 export class FavoritesComponent implements OnInit {
+  public filmsList: IFilmShort[];
 
-  constructor() { }
+  constructor(
+    public favoriteService: FavoriteService
+  ) { }
 
+  addToFavorite(film: IFilmShort) {
+    this.favoriteService.addToFavorite(film);
+    this.getFavorites();
+  }
+  favoriteCheck(film: IFilmShort) {
+    return this.favoriteService.checkFavorite(film);
+  }
+  getFavorites() {
+    this.filmsList = localStorage.favorite ? JSON.parse(localStorage.favorite) : [];
+  }
   ngOnInit() {
+    this.getFavorites();
   }
 
 }
