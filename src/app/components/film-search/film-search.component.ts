2061c5bb16b3421f1988c8039899c0e0c95e55cb
Fedorrychkov <fedorrychkov@yandex.ru>
2018-03-24 20:42:08 +0300

    Add favorites methods

--- src/app/components/film-search/film-search.component.ts
+++ src/app/components/film-search/film-search.component.ts
@@ -14,6 +14,7 @@ export class FilmSearchComponent implements OnInit {
   public filmTitle: FormControl = new FormControl();
   public filmTitlesList: String[] = [];
   public filmsList: IFilmShort[];
+  public favoriteList: IFilmShort[];
   public isLoading = true;
 
   constructor(
@@ -51,18 +52,34 @@ export class FilmSearchComponent implements OnInit {
       })
       .then(() => {
         this.isLoading = true;
-        console.log(this.filmsList);
       });
   }
 
   addToFavorite(film: IFilmShort) {
-    console.log(film);
-    // TODO: favorite add method
+    const favoriteList: IFilmShort[] = localStorage.favorite ? JSON.parse(localStorage.favorite) : [];
+    let list: IFilmShort[];
+    list = favoriteList.filter(item => item.imdbID !== film.imdbID );
+    if (!this.favoriteCheck(film)) {
+      list.push(film);
+    }
+    this.favoriteList = list;
+    localStorage.favorite = JSON.stringify(this.favoriteList);
   }
   favoriteCheck(film: IFilmShort) {
-    return true;
-    // TODO: favorite check method
+    const favoriteList: IFilmShort[] = localStorage.favorite ? JSON.parse(localStorage.favorite) : [];
+    let isFavorite = false;
+    favoriteList.forEach(item => {
+      if (item.imdbID === film.imdbID) {
+        isFavorite = true;
+      }
+    });
+    return isFavorite;
   }
+
+  // filter(val: IFilmShort): IFilmShort[] {
+  //   return this.favoriteList.filter(item =>
+  //     console.log(item));
+  // }
 
   ngOnInit() {
     this.SearchForm = this.fb.group({
